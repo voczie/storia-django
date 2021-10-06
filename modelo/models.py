ddd2a402123c25b9ca23fe12b4993caa6d632f43
voczie <mariagrisi@cc.ci.ufpb.br>
2021-10-06 11:08:47 -0300

    Models atualizado

--- modelo/models.py
+++ modelo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Story(models.Model):
+    SIZE_CHOICES = (('Small', 50), ('Medium', 100), ('Big', 200))
+    TEMPERATURE_CHOICES = (('Default', 1.0), ('Creative', 1.2), ('Very Creative', 1.5))
+
+    example = models.TextField()
+    size = models.IntegerField(choices=SIZE_CHOICES)
+    temperature = models.FloatField(choices=TEMPERATURE_CHOICES)
+
+    def __str__(self):
+        return "Example: " + str(self.example) + "\nTemperature: " + str(self.temperature) + "\nSize: " + str(self.size)
--- storia_django/settings.py
+++ storia_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'modelo'
+    'rest_framework',
+    'modelo',
 ]
 
 MIDDLEWARE = [
