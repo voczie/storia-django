ced511a22c1ffd004f511f77e4b334e4f6668652
voczie <mariagrisi@cc.ci.ufpb.br>
2021-10-06 00:05:27 -0300

    funcao oi kakaka

--- modelo/views.py
+++ modelo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def oi(request):
+    html = "<h1>sotira djano</h1>"
+    return HttpResponse(html)
