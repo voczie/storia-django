a138e8c8bb2a6dde1c8d55343d6dc1b968214a79
voczie <mariagrisi@cc.ci.ufpb.br>
2021-10-06 22:12:29 -0300

    Corrigido problema de rota /index /submit

--- modelo/urls.py
+++ modelo/urls.py
@@ -5,6 +5,7 @@ from . import views
 # URLConf
 urlpatterns = [
     path('index/', views.submit_form, name='index'),
+    path('submit/', views.submit_form, name='submit'),
     path('members/', views.members, name='members'),
     path('social/', views.social, name='social')
 ]
