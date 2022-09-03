e7c9229a00c8b5746b9d225bb2e333d1c701888d
Victoria <mariagrisi@cc.ci.ufpb.br>
2022-09-03 01:42:49 -0300

    Removendo coisas

--- storia_django/settings.py
+++ storia_django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h65+n#0oenpoo(z*1rgqjumi0+btee_#5+ax%w_)xw@u%#*=!1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
